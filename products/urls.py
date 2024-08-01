41a0606fad675c9d1e9c7775a7ec0dc6d9d31dd7
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-01 23:59:55 +0500

    rebased usersapi on ecommerce product apis

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from products.views import ProductListView,ProductByCategoryView, ProductBySubCategoryView
+from django.urls import path
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='product-list'),
+    path('subcategory/<int:subcategory_id>/', ProductBySubCategoryView.as_view(), name='product-by-subcategory'),
+    path('category/<int:category_id>/', ProductByCategoryView.as_view(), name='product-by-category'),
+]

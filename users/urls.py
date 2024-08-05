05af34783af67918242846d3ff80cc72ffd8c733
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-05 19:30:23 +0500

    rmeoved unsued code comments lines

--- users/urls.py
+++ users/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/signup/', SignupView.as_view(), name='signup'),
-    # path('auth/login/', LoginView.as_view(), name='login'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/password_change/', PasswordChangeView.as_view(), name='password-change'),
     path('users/', UsersListView.as_view(), name='users'),

3961b96dd36ccc555ae2116ac4710ce97d80c8bb
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-08 12:11:15 +0500

    removed commented code

--- basic_user_application/settings.py
+++ basic_user_application/settings.py
@@ -108,17 +108,6 @@ WSGI_APPLICATION = 'basic_user_application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ecommerce',
-#         'USER': 'postgres',
-#         'PASSWORD': '786allah',
-#         'HOST': 'localhost',
-#         'PORT': '5432',
-#     }
-# }
-
 
 DATABASES = {
     'default': {

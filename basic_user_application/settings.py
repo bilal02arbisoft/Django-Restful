87d8540472375e6fa59114fbe97e26e18474a422
Bilal Ahmad <bilalahmad02@arbisoft.com>
2024-08-08 12:08:18 +0500

    docker file for backend container and docker-compose.yml for postgres container and backend

--- basic_user_application/settings.py
+++ basic_user_application/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,14 +108,26 @@ WSGI_APPLICATION = 'basic_user_application.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'ecommerce',
+#         'USER': 'postgres',
+#         'PASSWORD': '786allah',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ecommerce',
-        'USER': 'postgres',
-        'PASSWORD': '786allah',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.getenv('SQL_DB'),
+        'USER': os.getenv('SQL_USER'),
+        'PASSWORD': os.getenv('SQL_PASSWORD'),
+        'HOST': os.getenv('SQL_HOST'),
+        'PORT': os.getenv('SQL_PORT'),
     }
 }
 
---
+++ entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+
+gunicorn basic_user_application.wsgi:application --bind 0.0.0.0:8000
+
+exec "$@"
